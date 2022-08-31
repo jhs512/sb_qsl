095d3dcc665e0dc4193c72b1912f5a98ea854aa8
jhs512 <jangka512@gmail.com>
2022-08-31 14:27:46 +0900

    inner join alias 를 통해서 조인 엔티티에 조건 삽입

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.ll.exam.qsl.user.repository;
 
+import com.ll.exam.qsl.interestKeyword.entity.QInterestKeyword;
 import com.ll.exam.qsl.user.entity.SiteUser;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
@@ -109,9 +110,14 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
        ON IK.content = SUIK.interest_keywords_content
        WHERE IK.content = "축구";
        */
+        QInterestKeyword IK = new QInterestKeyword("IK");
+
         return jpaQueryFactory
                 .selectFrom(siteUser)
-                .innerJoin(siteUser.interestKeywords)
+                .innerJoin(siteUser.interestKeywords, IK)
+                .where(
+                        IK.content.eq(keywordContent)
+                )
                 .fetch();
     }
 }
