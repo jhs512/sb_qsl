d59520acff19e2025bf0e350c871454d3dd87419
jhs512 <jangka512@gmail.com>
2022-09-02 14:09:21 +0900

    고아객체 제거

--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -264,7 +264,6 @@ class UserRepositoryTests {
 
     @Test
     @DisplayName("특정회원의 follower들과 following들을 모두 알 수 있어야 한다.")
-    @Rollback(false)
     void t15() {
         SiteUser u1 = userRepository.getQslUser(1L);
         SiteUser u2 = userRepository.getQslUser(2L);
@@ -286,5 +285,13 @@ class UserRepositoryTests {
         // following
         // u2가 구독중인 회원 : 0
         assertThat(u2.getFollowings().size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("u1은 더 이상 농구에 관심이 없습니다.")
+    void t16() {
+        SiteUser u1 = userRepository.getQslUser(1L);
+
+        u1.removeInterestKeywordContent("농구");
     }
 }
