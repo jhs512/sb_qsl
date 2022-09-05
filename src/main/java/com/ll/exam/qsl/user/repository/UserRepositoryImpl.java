82a0f0121c62693a1313777cbc317e99bca3b6f1
jhs512 <jangka512@gmail.com>
2022-09-05 13:32:42 +0900

    특정 회원이 팔로우 중인 회원들의 관심사

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.ll.exam.qsl.user.repository;
 
+import com.ll.exam.qsl.user.entity.QSiteUser;
 import com.ll.exam.qsl.user.entity.SiteUser;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
@@ -116,6 +117,20 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
                 .where(
                         interestKeyword.content.eq(keywordContent)
                 )
+                .fetch();
+    }
+
+    @Override
+    public List<String> getKeywordContentsByFollowingsOf(SiteUser user) {
+        QSiteUser siteUser2 = new QSiteUser("siteUser2");
+
+        return jpaQueryFactory
+                .select(interestKeyword.content)
+                .distinct()
+                .from(interestKeyword)
+                .innerJoin(interestKeyword.user, siteUser) // site_user
+                .innerJoin(siteUser.followers, siteUser2)
+                .where(siteUser2.id.eq(user.getId()))
                 .fetch();
     }
 }
